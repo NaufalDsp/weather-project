e2e2f6d07f5ea507d5e5d1795b3052d404cdfc82
NaufalDsp <145748689+NaufalDsp@users.noreply.github.com>
2026-02-10 19:17:38 +0700

    feat: refactor getWeather function to fetch current weather and forecast data

--- src/api.ts
+++ src/api.ts
@@ -1,9 +1,21 @@
 const API_KEY = import.meta.env.VITE_API_KEY;
 
 export async function getWeather({ lat, lon }: { lat: number; lon: number }) {
-  const res = await fetch(
-    `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,alerts&appid=${API_KEY}`,
-  )
-  const data = await res.json();
-  return data;
+  // Current Weather API
+  const currentWeather = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${API_KEY}`,
+  );
+
+  // 5 Day / 3 Hour Forecast API
+  const forecast = await fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${API_KEY}`,
+  );
+
+  const current = await currentWeather.json();
+  const forecastData = await forecast.json();
+
+  return {
+    current,
+    forecast: forecastData,
+  };
 }
